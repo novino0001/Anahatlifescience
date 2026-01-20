947403e3cac7d780d649cfc9d3640bfcd3bbef3a
novino0001 <novino.2k157@gmail.com>
2026-01-20 09:58:01 +0530

    responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+      </head>
       <body className={`${geist.variable} font-sans antialiased`}>{children}</body>
     </html>
   )
